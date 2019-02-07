23e675ca79d7bd3bd81a05cdf25214e9c2f65ac3
Tom Grayston <t.grayston@lancaster.ac.uk>
2019-02-07 17:51:44 +0000

    Complete Overhaul
    Made a new version, slimmed down the code

---
+++ VR_Dis_Task(NoEyeTrack)/Assets/Imported Files/VRSampleScenes/Scripts/Examples/ExampleInteractiveItem_target.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+namespace VRStandardAssets.Examples
+{
+    // This script is a simple example of how an interactive item can
+    // be used to change things on gameobjects by handling events.
+    public class ExampleInteractiveItem_target : MonoBehaviour
+    {
+        [SerializeField] private Material m_NormalMaterial;                
+        [SerializeField] private Material m_OverMaterial;                           
+        [SerializeField] private VRInteractiveItem m_InteractiveItem;
+        [SerializeField] private Renderer m_Renderer;
+        [SerializeField] public GameObject target;
+
+
+        private void Awake ()
+        {
+            m_Renderer.material = m_NormalMaterial;
+        }
+
+
+        private void OnEnable()
+        {
+            m_InteractiveItem.OnOver += HandleOver;
+            m_InteractiveItem.OnOut += HandleOut;
+        }
+
+
+        private void OnDisable()
+        {
+            m_InteractiveItem.OnOver -= HandleOver;
+            m_InteractiveItem.OnOut -= HandleOut;
+        }
+
+
+        //Handle the Over event
+        private void HandleOver()
+        {
+            Debug.Log("Show over state");
+            m_Renderer.material = m_OverMaterial;
+            Destroy(target);
+        }
+
+
+        //Handle the Out event
+        private void HandleOut()
+        {
+            Debug.Log("Show out state");
+            m_Renderer.material = m_NormalMaterial;
+        }
+    }
+
+}
